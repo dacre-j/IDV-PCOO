5b21658ffa0838166979d63b647f6766f3b163ec
Sayada <fsayada92@hotmail.com>
2012-11-28 15:37:46 +0100

    Ajout de Principale.java

---
+++ Principale.java
@@ -0,0 +1,13 @@
+public class Principale {
+	public void affiche(int j)
+	{
+		for (int i = 0; i < j; i++)
+		{
+			System.out.println("bonjour " + (i + 1) + " fois");
+		}
+
+		public static void main(String[] args) {
+			affiche(100);
+		}
+	}
+}
