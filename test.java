85810441979157a75a79bb346fecd8554b6be90b
dacre-_j <jean-eudes.dacre-wright@epitech.eu>
2012-11-28 15:14:33 +0100

    Copie de Principal

---
+++ test.java
@@ -0,0 +1,10 @@
+public class Principal{
+
+    public static void main (String[] args)
+    {
+	int i = 0;
+	for (; i < 10; i++)
+	    System.out.println("bonjour " + (i + 1) + " fois");
+	System.out.println("bonjour " + i + " fois");
+    }
+}
